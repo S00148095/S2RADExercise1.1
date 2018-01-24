5fc38b4dfc1f4a7e7efcca139784628d44b1879d
silentgolem <liamlogan16@yahoo.co.uk>
2018-01-24 11:45:14 +0000

    1.1 complete

---
+++ Exercise1a/Data/ContextInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Exercise1a.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Exercise1a.Data
+{
+    public class ContextInitializer : DropCreateDatabaseIfModelChanges<CoreContext>
+    {
+        protected override void Seed(CoreContext context)
+        {
+            var Users = new List<User>
+            {
+                new User(){ FirstName="John", MidName="Frank", LastName="Doe", DOB=DateTime.Parse("1980-01-01") },
+
+                new User(){ FirstName="Jane", MidName="Frank", LastName="Doe", DOB=DateTime.Parse("1986-01-01") },
+
+                new User(){ FirstName="Mark", MidName="Frank", LastName="Smith", DOB=DateTime.Parse("1972-01-01") },
+
+                new User(){ FirstName="Mary", MidName="Frank", LastName="Smith", DOB=DateTime.Parse("1975-01-01") },
+            };
+
+            Users.ForEach(u => context.Users.Add(u));
+            base.Seed(context);
+        }
+
+    }
+}
---
+++ Exercise1a/Data/CoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Exercise1a.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Exercise1a.Data
+{
+    public class CoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public CoreContext() : base("CoreContext") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
